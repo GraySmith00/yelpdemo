273dd66e2d106a3429ebf8c98da529a958f17020
Gray Smith <gray.smith00@gmail.com>
2015-07-21 17:00:44 +0000

    add validations to review.rb and restaurant.rb model files and add custom bootstrap styling to validation alerts in restaurant and review form partials

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,16 @@ class Restaurant < ActiveRecord::Base
   
   has_many :reviews
   
+  validates :name, :address, :phone, :website, :image, presence: true
+  
+  validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
+    message: "must be in the format (555) 555-5555" }
+  
+  validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+    message: "must start with http:// or https://" }
+  
+  validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
+    message: "must be in the format 555 Fifth Avenue, New York, NY 10118" }
+  
+  
 end
