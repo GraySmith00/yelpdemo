3a09e6a9d72e356ba4392149434fdd2d110c2cca
Gray Smith <gray.smith00@gmail.com>
2015-07-22 16:57:29 +0000

    add permissions to admins and create review edit and delete links

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,7 @@ class ReviewsController < ApplicationController
   before_action :set_review, only: [:edit, :update, :destroy]
   before_action :set_restaurant
   before_action :authenticate_user!
+  before_action :check_user, only: [:edit, :update, :destroy]
 
   respond_to :html
 
@@ -64,5 +65,11 @@ class ReviewsController < ApplicationController
 
     def review_params
       params.require(:review).permit(:rating, :comment)
+    end
+  
+    def check_user
+      unless (@review.user == current_user) || (current_user.admin?)
+        redirect_to root_url, alert: "Sorry, this review belongs to someone else"
+      end
     end
 end
